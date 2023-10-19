0ab8377cbb5721fe8fc0cbb621b87ddac87dda48
MaykonOliveiraC <maykonoliveiradev@gmail.com>
2023-10-19 09:57:49 -0300

    loja

--- loja/main/models.py
+++ loja/main/models.py
@@ -51,6 +51,15 @@ class Product(models.Model):
     status=models.BooleanField(default=True)
     
 
+class ProductAttribute(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    color = models.ForeignKey(Color,on_delete=models.CASCADE)
+    size = models.ForeignKey(Size,on_delete=models.CASCADE)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.product.title
+
 
 
 
